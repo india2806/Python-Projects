e594f87112e57b7d6d5bb5f0457dbacd003a8f38
india <indiabs2806@gmail.com>
2021-06-23 21:51:56 -0700

    commit changes

---
+++ voice translator and snake/voice translator.py
@@ -0,0 +1,37 @@
+import speech_recognition as sr
+from google_trans_new import google_translator
+import pyttsx3
+recognizer=sr.Recognizer()
+engine=pyttsx3.init()
+
+with sr.Microphone() as source:
+    print("Clearing the background noises.. ")
+    recognizer.adjust_for_ambient_noise(source,duration=1)
+    print("Waiting for your message")
+    audio= recognizer.listen(source,timeout=6)
+    print("Recording done!")
+
+try:
+    print("Recognizing")
+    result=recognizer.recognize_google(audio,language='en')
+except Exception as ex:
+    print(ex)
+
+#translational function
+
+def trans():
+    langinput=input("Type the language code you wanna translate:")
+    translator=google_translator()
+    translate_text=translator.translate(str(result),lang_tgt=str(langinput))
+    print(translate_text)
+    engine.say(str(translate_text))
+    engine.runAndWait()
+
+trans()
+
+
+
+
+
+
+
